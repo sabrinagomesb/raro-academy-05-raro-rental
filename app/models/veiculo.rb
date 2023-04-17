581547b3973eb988371b90d30cfd139d15935827
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-04-17 13:21:28 -0300

    feat(model): implementa os erros de Cliente e Veiculo

--- app/models/veiculo.rb
+++ app/models/veiculo.rb
@@ -2,4 +2,10 @@ class Veiculo < ApplicationRecord
   def to_s
     "#{modelo} #{placa}"
   end
+
+  validates_presence_of :marca
+  validates_presence_of :modelo
+  validates_presence_of :cor
+  validates :placa, length: { in: 7..7 }
+  validates :chassi, length: { in: 17..17 }
 end
