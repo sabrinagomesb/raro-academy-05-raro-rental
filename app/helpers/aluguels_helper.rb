7c3faffc20bc2ad1de5a27ef41976e0dd28e3130
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-04-18 17:59:09 -0300

    feat(helpers): implementa funções de formatar data e moeda

--- app/helpers/aluguels_helper.rb
+++ app/helpers/aluguels_helper.rb
@@ -1,2 +1,9 @@
 module AluguelsHelper
+  def date_format_br(date)
+    date.strftime "%d/%m/%Y"
+  end
+
+  def currency_format_brl(number)
+    number_to_currency(number, :unit => "R$ ", :separator => ",", :delimiter => ".")
+  end
 end
