2c2858d2265dd1ac37685f757afd6e626dc48b7e
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-04-18 11:21:17 -0300

    fix(view): adiciona placeholder ao input de valor pago

--- app/models/aluguel.rb
+++ app/models/aluguel.rb
@@ -6,4 +6,6 @@ class Aluguel < ApplicationRecord
   validates_presence_of :data_fim
   validates_presence_of :valor_pago
   validates :data_fim, comparison: { greater_than: :data_inicio }
+
+  # validates :valor_pago, format: { with: /\A\d+(\.\d{0,2})?\z/ }
 end
