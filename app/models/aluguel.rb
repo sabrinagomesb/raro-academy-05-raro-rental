e1c35d8f24cfe7eed6e31419de994e3f1d00a5df
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-04-18 09:23:16 -0300

    feat(model): implementa as validaçes de Aluguel

--- app/models/aluguel.rb
+++ app/models/aluguel.rb
@@ -1,4 +1,9 @@
 class Aluguel < ApplicationRecord
   belongs_to :cliente
   belongs_to :veiculo
+
+  validates_presence_of :data_inicio
+  validates_presence_of :data_fim
+  validates_presence_of :valor_pago
+  validates :data_fim, comparison: { greater_than: :data_inicio }
 end
