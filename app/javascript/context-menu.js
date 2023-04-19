3edb22e3af163376ccf574b2af5bd85ba8baec63
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-04-19 00:31:14 -0300

    refactor(view): corrige menu de contexto do usuario

--- app/javascript/context-menu.js
+++ app/javascript/context-menu.js
@@ -1,22 +1,13 @@
-const toggleUserMenu = (event) => {
-  const userMenu = document.querySelector('.context-menu');
-
-  if (userMenu.classList.contains('hidden')) {
-    userMenu.classList.remove('hidden')
+const toggleMenu = (event) => {
+  const menu = document.querySelector('.context-menu');
+  if (menu.classList.contains('hidden')) {
+    menu.classList.remove('hidden')
   } else {
-    userMenu.classList.add('hidden')
+    menu.classList.add('hidden')
   }
 
-  // este comando interrompe a propagação de eventos para os demais elementos pais do #open-user-menu.
-  // se eu não coloco este filtro, o evento para fechar o menu de contexto declarado para o docucment
-  // seria disparado, e por consequencia, nunca veriamos este menu aberto.
   event.stopPropagation();
 };
 
-// // selecionando os objetos da tela para adicionar os eventos de click.
-const openUserMenu = document.querySelector('#open-user-menu');
-
-// // ligandos os eventos da tela.
-openUserMenu.addEventListener('click', toggleUserMenu);
-
-
+const userMenu = document.querySelector('#open-user-menu');
+userMenu.addEventListener('click', toggleMenu);
