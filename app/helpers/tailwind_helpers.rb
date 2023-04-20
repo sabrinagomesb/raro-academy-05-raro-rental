a7c4634911b6799a943a58e38f923a35328d3403
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-04-19 23:19:27 -0300

    refactor(view): adiciona checkbox de root

--- app/helpers/tailwind_helpers.rb
+++ app/helpers/tailwind_helpers.rb
@@ -49,6 +49,10 @@ module TailwindHelpers
     "rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
   end
 
+  def input_checkbox
+    "ml-1 h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+  end
+
   # Dropdown menu
   def dropdown_menu_item
     "block px-4 py-2 text-sm text-gray-700"
